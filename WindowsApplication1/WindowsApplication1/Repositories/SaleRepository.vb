d079b31f615f7f247bfbb3fe236ba27cc73db3cc
Felixander <felixanderdoloso90@gmail.com>
2026-05-17 21:07:55 +0800

    Update SaleRepository.vb

--- WindowsApplication1/WindowsApplication1/Repositories/SaleRepository.vb
+++ WindowsApplication1/WindowsApplication1/Repositories/SaleRepository.vb
@@ -1,3 +1,82 @@
-﻿Public Class SaleRepository
+﻿Imports MySql.Data.MySqlClient
+Imports Point_of_Sale_System.Data
+Imports Point_of_Sale_System.Models
 
-End Class
+Namespace Repositories
+
+    Public Class SaleRepository
+
+        Private ReadOnly _factory As New DbConnectionFactory()
+
+        Public Function Save(sale As Sale) As Integer
+
+            Using connection = _factory.CreateConnection()
+
+                connection.Open()
+
+                Using transaction = connection.BeginTransaction()
+
+                    Try
+
+                        Using command As New MySqlCommand(
+                            "INSERT INTO sales (cashier_id, total_amount)
+                     VALUES (@cashierId, @total);
+                     SELECT LAST_INSERT_ID();",
+                             connection,
+                             transaction)
+
+                            command.Parameters.AddWithValue("@cashierId", sale.CashierID)
+
+                            command.Parameters.AddWithValue("@total", sale.TotalAmount)
+
+                            sale.Id = Convert.ToInt32(command.ExecuteScalar())
+
+                        End Using
+
+                        For Each item In sale.Items
+
+                            Using itemCommand As New MySqlCommand(
+                                "INSERT INTO sale_items
+                         (sale_id, product_id, quantity, unit_price, line_total)
+                         VALUES
+                         (@saleId, @productId, @quantity, @unitPrice, @lineTotal)",
+                                 connection,
+                                 transaction)
+
+                                itemCommand.Parameters.AddWithValue("@saleId", sale.Id)
+
+                                itemCommand.Parameters.AddWithValue("@productId", item.ProductId)
+
+                                itemCommand.Parameters.AddWithValue("@quantity", item.Quantity)
+
+                                itemCommand.Parameters.AddWithValue("@unitPrice", item.UnitPrice)
+
+                                itemCommand.Parameters.AddWithValue("@lineTotal", item.LineTotal)
+
+                                itemCommand.ExecuteNonQuery()
+
+                            End Using
+
+                        Next
+
+                        transaction.Commit()
+
+                        Return sale.Id
+
+                    Catch
+
+                        transaction.Rollback()
+
+                        Throw
+
+                    End Try
+
+                End Using
+
+            End Using
+
+        End Function
+
+    End Class
+
+End Namespace
