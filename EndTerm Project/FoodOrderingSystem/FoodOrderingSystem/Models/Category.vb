20ccade432381831a5563f7a1836dbffeda64fd4
Felixander <felixanderdoloso90@gmail.com>
2026-06-21 10:05:30 +0800

    Updated 06/21/26 (10:05)

--- EndTerm Project/FoodOrderingSystem/FoodOrderingSystem/Models/Category.vb
+++
@@ -1,8 +0,0 @@
-﻿Namespace Models
-    Public Class Category
-        Public Property Id As Integer
-        Public Property Name As String
-        Public Property Description As String
-        Public Property IsActive As Boolean
-    End Class
-End Namespace
--- EndTerm Project/FoodOrderingSystem/FoodOrderingSystem/Services/Session.vb
+++
@@ -1,5 +0,0 @@
-﻿Namespace Services
-    Public Module Session
-        Public Property CurrentUser As FoodOrderingSystem.Models.AppUser
-    End Module
-End Namespace
